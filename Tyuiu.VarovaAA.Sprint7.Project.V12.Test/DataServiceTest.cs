5aaa492c43b73625066f2936a93beaaa22e55570
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-12-18 15:50:16 +0500

    Add project files.

---
+++ Tyuiu.VarovaAA.Sprint7.Project.V12.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+using Tyuiu.VarovaAA.Sprint7.Project.V12.Lib;
+namespace Tyuiu.VarovaAA.Sprint7.Project.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\Users\User\source\repos\Tyuiu.VarovaAA.Sprint7.Project.V12\Tyuiu.VarovaAA.Sprint7.Project.V12\bin\Release\InPutDataFile.csv";
+
+            int row = 1;
+            int column = 1;
+            string res = ds.CollectTextFromFile(path, row, column);
+
+            string wait = "BoDe-WDH9";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VarovaAA.Sprint7.Project.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VarovaAA.Sprint7.Project.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VarovaAA.Sprint7.Project.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5265a5c8-9c89-4909-a91f-8b43ad4cd0ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
