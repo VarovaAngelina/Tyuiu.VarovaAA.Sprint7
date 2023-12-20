f5f0f093ca95e400c7f435d17a95517a52951ecc
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-12-20 14:57:15 +0500

    edit

--- Tyuiu.VarovaAA.Sprint7.Project.V12/FormMain.Designer.cs
+++ Tyuiu.VarovaAA.Sprint7.Project.V12/FormMain.Designer.cs
@@ -29,10 +29,17 @@ namespace Tyuiu.VarovaAA.Sprint7.Project.V12
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(976, 522);
+            this.Name = "FormMain";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
